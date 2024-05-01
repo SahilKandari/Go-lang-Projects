60b7f2776409057062f86141ad9632ae324d1e75
Sahil Kandari <sahil@Sahils-MacBook-Air.local>
2024-05-02 00:20:27 +0530

    Database added

--- main.go
+++ main.go
@@ -1,139 +1,112 @@
 package main
 
 import (
-	"encoding/json"
+	"database/sql"
 	"fmt"
 	"log"
-	"math/rand"
-	"net/http"
 	"strconv"
-	"time"
+
+	_ "github.com/lib/pq"
 )
 
-//Data model
-
-type User struct {
-	Id   string `json: "id"`
-	Name string `json: "name"`
+type Product struct {
+	Name      string
+	Price     float64
+	Available bool
 }
-
-type Tweet struct {
-	Id       string `json: "id"`
-	AuthorId string `json: "authorId"`
-	Message  string `json: "message"`
-}
-
-func (t Tweet) IsEmpty() bool {
-	return t.Message == ""
-}
-
-func (t Tweet) IsUserPresent(u []User) bool {
-	for _, user := range u {
-		if t.AuthorId == user.Id {
-			return true
-		}
-	}
-	return false
-}
-
-var users []User
-var tweets []Tweet
 
 func main() {
-	fmt.Println("Api Tweet")
+	conStr := "postgres://postgres:secret@localhost:5432/gopgtest?sslmode=disable"
 
-	//Seed Data
-	users = append(users, User{Id: "1", Name: "Sahil"}, User{Id: "2", Name: "Ritul"}, User{Id: "3", Name: "Sam"}, User{Id: "4", Name: "John"}, User{Id: "5", Name: "Doe"})
-	tweets = append(tweets, Tweet{Id: "1", AuthorId: "1", Message: "My first tweet"}, Tweet{Id: "2", AuthorId: "2", Message: "My second tweet"}, Tweet{Id: "3", AuthorId: "3", Message: "My third tweet"}, Tweet{Id: "4", AuthorId: "4", Message: "My fourth tweet"}, Tweet{Id: "5", AuthorId: "5", Message: "My fifth tweet"})
-
-	http.HandleFunc("/", serveHome)
-	http.HandleFunc("/listtweets", listTweets)
-	http.HandleFunc("/createtweet", createTweet)
-	http.HandleFunc("/getTweets/{id}", getTweets)
-
-	log.Fatal(http.ListenAndServe(":5000", nil))
-}
-
-func serveHome(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("Welcome to the Tweet Api")
-	w.Write([]byte("Welcome to the Tweet Api"))
-}
-
-func listTweets(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("Get all tweets")
-	w.Header().Set("Content-Type", "application/json")
-
-	err := json.NewEncoder(w).Encode(tweets)
-	if err != nil {
-		http.Error(w, "Internal server error while encoding response", http.StatusInternalServerError)
-	}
-}
-
-func createTweet(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("Create a tweet")
-	w.Header().Set("Content-Type", "application/json")
-
-	if r.Method != "POST" {
-		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
-		return
-	}
-
-	var tweet Tweet
-	err := json.NewDecoder(r.Body).Decode(&tweet)
+	db, err := sql.Open("postgres", conStr)
 
 	if err != nil {
-		http.Error(w, "Internal Server Error while decoding the JSON", http.StatusInternalServerError)
-		return
+		log.Fatal(err)
 	}
 
-	if tweet.IsEmpty() {
-		http.Error(w, "Tweet message is empty", http.StatusNotFound)
-		return
+	defer db.Close()
+
+	if err = db.Ping(); err != nil {
+		log.Fatal(err)
 	}
 
-	if tweet.IsUserPresent(users) == false {
-		http.Error(w, "User does not exist", http.StatusNotFound)
-		return
-	}
+	createProductTable(db)
 
-	rand.New(rand.NewSource(time.Now().UnixNano()))
-	tweet.Id = strconv.Itoa(rand.Intn(100))
+	product := Product{"Book", 19.99, true}
 
-	tweets = append(tweets, tweet)
+	pk := insertProduct(db, product)
 
-	respErr := json.NewEncoder(w).Encode(tweet)
-	if respErr != nil {
-		http.Error(w, "Internal server error while encoding response", http.StatusInternalServerError)
+	fmt.Printf("ID:=%v\n", pk)
+
+	queryRow(db, pk)
+
+	queryTable(db)
+}
+
+func createProductTable(db *sql.DB) {
+	query := `CREATE TABLE IF NOT EXISTS product (
+		id SERIAL PRIMARY KEY,
+		name VARCHAR(100) NOT NULL,
+		price NUMERIC(6, 2) NOT NULL,
+		available BOOLEAN,
+		created timestamp DEFAULT NOW()
+	)`
+
+	_, err := db.Exec(query)
+	if err != nil {
+		log.Fatal(err)
 	}
 }
 
-func getTweets(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("Get user tweets")
-	w.Header().Set("Content-Type", "application/json")
+func insertProduct(db *sql.DB, product Product) int {
+	query := `INSERT INTO product (name, price, available)
+	VALUES ($1, $2, $3) RETURNING id`
 
-	// params := strings.Split(r.URL.Path, "/")
-	// userId := params[len(params)-1]
-
-	userId := r.PathValue("id")
-
-	if userId == "" {
-		http.Error(w, "Please provide the User Id", http.StatusNotFound)
-		return
+	var pk int
+	err := db.QueryRow(query, product.Name, product.Price, product.Available).Scan(&pk)
+	if err != nil {
+		log.Fatal(err)
 	}
+	return pk
+}
 
-	userTweets := []Tweet{}
-	for _, tweet := range tweets {
-		if tweet.AuthorId == userId {
-			userTweets = append(userTweets, tweet)
+func queryRow(db *sql.DB, pk int) {
+	var name string
+	var price float64
+	var available bool
+
+	query := `SELECT name, price, available FROM product WHERE id = $1`
+
+	err := db.QueryRow(query, pk).Scan(&name, &price, &available)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			log.Fatal("No rows found with the id " + strconv.Itoa(pk))
 		}
+		log.Fatal(err)
+	}
+	fmt.Printf("Name:= %v, Price:= %v, Available:= %v\n", name, price, available)
+}
+
+func queryTable(db *sql.DB) {
+	data := []Product{}
+
+	query := `SELECT name, price, available FROM product`
+	rows, err := db.Query(query)
+
+	if err != nil {
+		log.Fatal(err)
 	}
 
-	if len(userTweets) > 0 {
-		json.NewEncoder(w).Encode(userTweets)
-		return
+	var name string
+	var price float64
+	var available bool
+
+	for rows.Next() {
+		err := rows.Scan(&name, &price, &available)
+		if err != nil {
+			log.Fatal(err)
+		}
+		data = append(data, Product{name, price, available})
 	}
-	err := json.NewEncoder(w).Encode("Unable to find tweets with the given user id")
-	if err != nil {
-		http.Error(w, "Internal server error while encoding response", http.StatusInternalServerError)
-	}
+	fmt.Println(data, "Data")
 }
